5057eb5d7c52f7b4fc18a258260a9a8557d6a72b
stephaneshao <stephaneshao@g.harvard.edu>
2018-05-02 23:19:17 +0100

    added example to manipulate trees in R without requiring C++ Tree module

--- inst/tutorial_tree.R
+++ inst/tutorial_tree.R
@@ -39,3 +39,46 @@ for (i in 1:N){
 }
 # plot all the trajectories
 matplot(t(paths), type = "l", col = rgb(0,0,0, alpha = 0.2), lty = 1)
+title("Paths (in C++)")
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Same task as previously using a tree implemented in R with no dependence on
+# the C++ Tree module
+
+# fix the random seed
+set.seed(17)
+# we illustrate the use of the class via a simple particle filter algorithm storing the surviving trajectories
+N <- 100 # number of particles
+T <- 10 # number of observations
+observations <- rnorm(T)
+# model:
+# x_1 ~ Normal(0,1); x_t | x_{t-1} ~ Normal(rho x_{t-1}, 1) where rho = 0.9
+# y_t | x_t ~ Normal(x_t, 1)
+# first particles
+# WARNING: particles must be formated as dimX by N matrices
+x <- matrix(rnorm(N, mean = 0, sd = 1), ncol = N)
+# initialize the tree
+Tree_R = tree_init(x)
+# reweight and resample
+weights <- dnorm(x = observations[1], mean = x, sd = 1)
+weights <- weights / sum(weights)
+ancestors <- sample(x = 1:N, size = N, replace = T, prob = weights)
+for (t in 2:T){
+  x <- matrix(0.9 * x[ancestors] + rnorm(N, mean = 0, sd = 1), ncol = N)
+  weights <- dnorm(x = observations[1], mean = x, sd = 1)
+  weights <- weights / sum(weights)
+  Tree_R = tree_update(Tree_R, x, ancestors)
+  ancestors <- sample(x = 1:N, size = N, replace = T, prob = weights)
+}
+# Now we can query the tree, for instance, getting all the trajectories
+paths_R <- matrix(0, nrow = N, ncol = T)
+for (i in 1:N){
+  paths_R[i,] <- tree_getpath(Tree_R,i)
+}
+# plot all the trajectories
+matplot(t(paths_R), type = "l", col = rgb(0,0,0, alpha = 0.2), lty = 1)
+title("Paths (in R)")
+
+# Check that both implementations give the same paths
+all(paths == paths_R)
